33a61a50ceeecab863f8a1f3a5072f95c4125f77
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-07 10:20:49 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+class TitleValidator < ActiveModel::Validator
+  def validate(record)
+    if record.title
+      unless record.title.match(/((Won't Believe)|(Secret)|(Top \d+)|(Guess))/i)
+        record.errors[:title] << 'Not Sufficiently Click-Baity'
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates_with TitleValidator
 end
