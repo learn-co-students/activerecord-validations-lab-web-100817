9947b713e2fcf4db62d58a0cffb86b5af90d3fd5
github username <github email address>
2017-11-07 11:45:35 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, {uniqueness: true, presence: true}
+  validates :phone_number, {uniqueness: true, length: {is: 10}}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
+  validate :clickbait?
+  
+  def clickbait?
+    clickbait_titles = ["Won't Believe", "Secret", "Top",  "Guess"]
+    is_valid = false
+    if self.title == nil
+      return false
+    end
+    clickbait_titles.each do |phrase|
+      if self.title.include?(phrase)
+        return true
+      end
+    end
+    if !is_valid 
+      self.errors[:title] << "This title isn't clickbaity enought!"
+    end
+    is_valid
+
+  end
 end
