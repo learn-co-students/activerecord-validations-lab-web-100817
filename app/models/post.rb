4141d9996ec725e022f002fcbbc057cfb69c348a
MatteBru <mbruni616@gmail.com>
2017-11-07 10:45:18 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  validates_presence_of :title
+  validates_length_of :content, minimum: 250
+  validates_length_of :summary, maximum: 250
+  validates_inclusion_of :category, in: ['Fiction', 'Non-Fiction']
+
+  validate :clickbait
+
+  def clickbait
+    errors.add(:base, 'not baity enough') if !self.title || !baity
+  end
+
+  def baity
+    cba = ["Won't Believe","Secret","Top[number]","Guess"]
+    cba.each { |cb| return true if self.title.include?(cb)}
+    false
+  end
+
 end
